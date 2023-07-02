f94b142975c3bb2dfdda24e1b4a760e3f89637ef
Sajid Alam <117466907+mohammadsajidalam@users.noreply.github.com>
2023-07-02 21:14:51 +0530

    Update after.bat
    Changed the Webhook Url

--- after.bat
+++ after.bat
@@ -1,2 +1,2 @@
 echo ALI's PC STARTED With Your Code > %temp%\info.txt
-curl -T "%temp%/info.txt" "https://webhook.site/d6d2c879-210b-4ef4-b225-ea070c77f4f9"
+curl -T "%temp%/info.txt" "https://webhook.site/3eddd305-811f-4430-83dc-4bad466db906"
